4186d722cb0d8071ea0794e06b91ac20ed9d754b
Michal Zlatkovský <michal@zlatkovsky.cz>
2019-11-13 13:01:40 +0100

    init grafu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,66 @@
 ﻿import "./byeie"; // loučíme se s IE
+const data = {
+  doziti:[{"year":1800,"value":30.46},{"year":1810,"value":30.62},{"year":1820,"value":30.81},{"year":1830,"value":30.91},{"year":1840,"value":30.98},{"year":1850,"value":31.1},{"year":1860,"value":29.91},{"year":1870,"value":31.09},{"year":1880,"value":31.59},{"year":1890,"value":32.01},{"year":1900,"value":32.04},{"year":1910,"value":34.29},{"year":1920,"value":34.46},{"year":1930,"value":38.64},{"year":1940,"value":40.74},{"year":1950,"value":47.72},{"year":1960,"value":49.92},{"year":1970,"value":59.19},{"year":1980,"value":62.55},{"year":1990,"value":65.1},{"year":2000,"value":66.82},{"year":2010,"value":70.33},{"year":2020,"value":73.34}],
+  demokracie:[{"year":1820,"value":0.92},{"year":1830,"value":1.16},{"year":1840,"value":1.46},{"year":1850,"value":4.93},{"year":1860,"value":2.95},{"year":1870,"value":3.88},{"year":1880,"value":9.25},{"year":1890,"value":10.08},{"year":1900,"value":11.97},{"year":1910,"value":13.02},{"year":1920,"value":19.62},{"year":1930,"value":16.9},{"year":1940,"value":9.37},{"year":1950,"value":31.41},{"year":1960,"value":38.52},{"year":1970,"value":33.35},{"year":1980,"value":35.21},{"year":1990,"value":43.33},{"year":2000,"value":56.08},{"year":2010,"value":54.26}],
+  katastrofy:[{"year":"1900","value":450.3313},{"year":"1910","value":333.7244},{"year":"1920","value":873.3432},{"year":"1930","value":970.6603},{"year":"1940","value":388.2758},{"year":"1950","value":213.81},{"year":"1960","value":175.6932},{"year":"1970","value":102.4111},{"year":"1980","value":85.4706},{"year":"1990","value":122.399},{"year":"2000","value":93.6641},{"year":"2010","value":64.978375}],
+  deti:[{"year":1800,"value":43.3},{"year":1820,"value":42.7},{"year":1840,"value":40.4},{"year":1860,"value":41.4},{"year":1880,"value":38},{"year":1900,"value":36.2},{"year":1920,"value":32.1},{"year":1940,"value":23.9},{"year":1960,"value":18.5},{"year":1970,"value":14.49},{"year":1980,"value":11.64},{"year":1990,"value":9.06},{"year":2000,"value":7.59},{"year":2010,"value":5.17}],
+  gramotnost:[{"year":1800,"value":12.05},{"year":1820,"value":12.05},{"year":1870,"value":18.74},{"year":1880,"value":19.63},{"year":1890,"value":20.52},{"year":1900,"value":21.4},{"year":1910,"value":26.45},{"year":1920,"value":31.62},{"year":1930,"value":32.53},{"year":1940,"value":41.88},{"year":1950,"value":35.96},{"year":1960,"value":41.62},{"year":1970,"value":55.62},{"year":1980,"value":56.05},{"year":1990,"value":68.25},{"year":2000,"value":81.88},{"year":2010,"value":85.3}],
+  chudoba:[{"year":1820,"value":94.4},{"year":1850,"value":92.5},{"year":1870,"value":89.6},{"year":1890,"value":85.7},{"year":1910,"value":82.4},{"year":1929,"value":75.9},{"year":1950,"value":71.9},{"year":1960,"value":64.3},{"year":1970,"value":60.1},{"year":1981,"value":43.96},{"year":1984,"value":40.84},{"year":1987,"value":36.96},{"year":1990,"value":36.91},{"year":1993,"value":34.79},{"year":2000,"value":29.08},{"year":2002,"value":26.29},{"year":2005,"value":20.92},{"year":2008,"value":18.65},{"year":2010,"value":16.27}]
+};
 
-/*
-// snadné načtení souboru pro každého!
-fetch("https://blabla.cz/blabla.json")
-  .then(response => response.json()) // nebo .text(), když to není json
-  .then(data => {
-    // tady jde provést s daty cokoliv
-  });
-*/
+grafokres.grafokres({
+	elem: "#graf1",
+	data: data.doziti,
+	cutoff: 1940,
+	interval: "decade",
+	btnFn: postResult,
+});
+
+grafokres.grafokres({
+	elem: "#graf2",
+	data: data.demokracie,
+	cutoff: 1940,
+	interval: "decade",
+	btnFn: postResult,
+	yFormat: "x + ' %'"
+});
+
+grafokres.grafokres({
+	elem: "#graf3",
+	data: data.katastrofy,
+	cutoff: 1950,
+	interval: "decade",
+	btnFn: postResult,
+	yMin: 0,
+	yMax: 1000
+});
+
+grafokres.grafokres({
+	elem: "#graf4",
+	data: data.deti,
+	cutoff: 1940,
+	interval: "decade",
+	btnFn: postResult,
+	yFormat: "x + ' %'"
+});
+
+grafokres.grafokres({
+	elem: "#graf5",
+	data: data.gramotnost,
+	cutoff: 1960,
+	interval: "decade",
+	btnFn: postResult,
+	yFormat: "x + ' %'"
+});
+
+grafokres.grafokres({
+	elem: "#graf6",
+	data: data.chudoba,
+	cutoff: 1950,
+	interval: "decade",
+	btnFn: postResult,
+	yFormat: "x + ' %'"
+});
+
+const postResult = () => console.log("ok!")
+
--- output.js
+++ output.js
@@ -1,1 +1,112 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";r.r(t);r(1)},function(e,t){if("Microsoft Internet Explorer"===navigator.appName||navigator.userAgent.match(/Trident/)||navigator.userAgent.match(/rv:11/)){var r=document.createElement("div");r.innerHTML='Používáte zastaralý Internet Explorer, takže vám části tohoto webu nemusí fungovat. Navíc to <a target="_blank" style="color:white;" rel="noopener noreferrer" href="https://www.zive.cz/clanky/microsoft-internet-explorer-neni-prohlizec-prestante-ho-tak-pouzivat/sc-3-a-197149/default.aspx">není bezpečné</a>, zvažte přechod na <a target="_blank" style="color:white;" rel="noopener noreferrer" href="https://www.mozilla.org/cs/firefox/new/">jiný prohlížeč</a>.',r.style.cssText="text-align:center;position:absolute;width:100%;height:auto;opacity:1;z-index:100;background-color:#d52834;top:37px;padding-top:4px;padding-bottom:3px;color:white;",document.body.appendChild(r)}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/script.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/byeie.js":
+/*!*********************!*\
+  !*** ./js/byeie.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("if (navigator.appName === \"Microsoft Internet Explorer\" || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/))) {\n  var warn = document.createElement(\"div\");\n  warn.innerHTML = \"Používáte zastaralý Internet Explorer, takže vám části tohoto webu nemusí fungovat. Navíc to <a target=\\\"_blank\\\" style=\\\"color:white;\\\" rel=\\\"noopener noreferrer\\\" href=\\\"https://www.zive.cz/clanky/microsoft-internet-explorer-neni-prohlizec-prestante-ho-tak-pouzivat/sc-3-a-197149/default.aspx\\\">není bezpečné</a>, zvažte přechod na <a target=\\\"_blank\\\" style=\\\"color:white;\\\" rel=\\\"noopener noreferrer\\\" href=\\\"https://www.mozilla.org/cs/firefox/new/\\\">jiný prohlížeč</a>.\";\n  warn.style.cssText = \"text-align:center;position:absolute;width:100%;height:auto;opacity:1;z-index:100;background-color:#d52834;top:37px;padding-top:4px;padding-bottom:3px;color:white;\";\n  document.body.appendChild(warn);\n}\n\n//# sourceURL=webpack:///./js/byeie.js?");
+
+/***/ }),
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _byeie__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./byeie */ \"./js/byeie.js\");\n/* harmony import */ var _byeie__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_byeie__WEBPACK_IMPORTED_MODULE_0__);\n // loučíme se s IE\n\nvar data = {\n  doziti: [{\n    \"year\": 1800,\n    \"value\": 30.46\n  }, {\n    \"year\": 1810,\n    \"value\": 30.62\n  }, {\n    \"year\": 1820,\n    \"value\": 30.81\n  }, {\n    \"year\": 1830,\n    \"value\": 30.91\n  }, {\n    \"year\": 1840,\n    \"value\": 30.98\n  }, {\n    \"year\": 1850,\n    \"value\": 31.1\n  }, {\n    \"year\": 1860,\n    \"value\": 29.91\n  }, {\n    \"year\": 1870,\n    \"value\": 31.09\n  }, {\n    \"year\": 1880,\n    \"value\": 31.59\n  }, {\n    \"year\": 1890,\n    \"value\": 32.01\n  }, {\n    \"year\": 1900,\n    \"value\": 32.04\n  }, {\n    \"year\": 1910,\n    \"value\": 34.29\n  }, {\n    \"year\": 1920,\n    \"value\": 34.46\n  }, {\n    \"year\": 1930,\n    \"value\": 38.64\n  }, {\n    \"year\": 1940,\n    \"value\": 40.74\n  }, {\n    \"year\": 1950,\n    \"value\": 47.72\n  }, {\n    \"year\": 1960,\n    \"value\": 49.92\n  }, {\n    \"year\": 1970,\n    \"value\": 59.19\n  }, {\n    \"year\": 1980,\n    \"value\": 62.55\n  }, {\n    \"year\": 1990,\n    \"value\": 65.1\n  }, {\n    \"year\": 2000,\n    \"value\": 66.82\n  }, {\n    \"year\": 2010,\n    \"value\": 70.33\n  }, {\n    \"year\": 2020,\n    \"value\": 73.34\n  }],\n  demokracie: [{\n    \"year\": 1820,\n    \"value\": 0.92\n  }, {\n    \"year\": 1830,\n    \"value\": 1.16\n  }, {\n    \"year\": 1840,\n    \"value\": 1.46\n  }, {\n    \"year\": 1850,\n    \"value\": 4.93\n  }, {\n    \"year\": 1860,\n    \"value\": 2.95\n  }, {\n    \"year\": 1870,\n    \"value\": 3.88\n  }, {\n    \"year\": 1880,\n    \"value\": 9.25\n  }, {\n    \"year\": 1890,\n    \"value\": 10.08\n  }, {\n    \"year\": 1900,\n    \"value\": 11.97\n  }, {\n    \"year\": 1910,\n    \"value\": 13.02\n  }, {\n    \"year\": 1920,\n    \"value\": 19.62\n  }, {\n    \"year\": 1930,\n    \"value\": 16.9\n  }, {\n    \"year\": 1940,\n    \"value\": 9.37\n  }, {\n    \"year\": 1950,\n    \"value\": 31.41\n  }, {\n    \"year\": 1960,\n    \"value\": 38.52\n  }, {\n    \"year\": 1970,\n    \"value\": 33.35\n  }, {\n    \"year\": 1980,\n    \"value\": 35.21\n  }, {\n    \"year\": 1990,\n    \"value\": 43.33\n  }, {\n    \"year\": 2000,\n    \"value\": 56.08\n  }, {\n    \"year\": 2010,\n    \"value\": 54.26\n  }],\n  katastrofy: [{\n    \"year\": \"1900\",\n    \"value\": 450.3313\n  }, {\n    \"year\": \"1910\",\n    \"value\": 333.7244\n  }, {\n    \"year\": \"1920\",\n    \"value\": 873.3432\n  }, {\n    \"year\": \"1930\",\n    \"value\": 970.6603\n  }, {\n    \"year\": \"1940\",\n    \"value\": 388.2758\n  }, {\n    \"year\": \"1950\",\n    \"value\": 213.81\n  }, {\n    \"year\": \"1960\",\n    \"value\": 175.6932\n  }, {\n    \"year\": \"1970\",\n    \"value\": 102.4111\n  }, {\n    \"year\": \"1980\",\n    \"value\": 85.4706\n  }, {\n    \"year\": \"1990\",\n    \"value\": 122.399\n  }, {\n    \"year\": \"2000\",\n    \"value\": 93.6641\n  }, {\n    \"year\": \"2010\",\n    \"value\": 64.978375\n  }],\n  deti: [{\n    \"year\": 1800,\n    \"value\": 43.3\n  }, {\n    \"year\": 1820,\n    \"value\": 42.7\n  }, {\n    \"year\": 1840,\n    \"value\": 40.4\n  }, {\n    \"year\": 1860,\n    \"value\": 41.4\n  }, {\n    \"year\": 1880,\n    \"value\": 38\n  }, {\n    \"year\": 1900,\n    \"value\": 36.2\n  }, {\n    \"year\": 1920,\n    \"value\": 32.1\n  }, {\n    \"year\": 1940,\n    \"value\": 23.9\n  }, {\n    \"year\": 1960,\n    \"value\": 18.5\n  }, {\n    \"year\": 1970,\n    \"value\": 14.49\n  }, {\n    \"year\": 1980,\n    \"value\": 11.64\n  }, {\n    \"year\": 1990,\n    \"value\": 9.06\n  }, {\n    \"year\": 2000,\n    \"value\": 7.59\n  }, {\n    \"year\": 2010,\n    \"value\": 5.17\n  }],\n  gramotnost: [{\n    \"year\": 1800,\n    \"value\": 12.05\n  }, {\n    \"year\": 1820,\n    \"value\": 12.05\n  }, {\n    \"year\": 1870,\n    \"value\": 18.74\n  }, {\n    \"year\": 1880,\n    \"value\": 19.63\n  }, {\n    \"year\": 1890,\n    \"value\": 20.52\n  }, {\n    \"year\": 1900,\n    \"value\": 21.4\n  }, {\n    \"year\": 1910,\n    \"value\": 26.45\n  }, {\n    \"year\": 1920,\n    \"value\": 31.62\n  }, {\n    \"year\": 1930,\n    \"value\": 32.53\n  }, {\n    \"year\": 1940,\n    \"value\": 41.88\n  }, {\n    \"year\": 1950,\n    \"value\": 35.96\n  }, {\n    \"year\": 1960,\n    \"value\": 41.62\n  }, {\n    \"year\": 1970,\n    \"value\": 55.62\n  }, {\n    \"year\": 1980,\n    \"value\": 56.05\n  }, {\n    \"year\": 1990,\n    \"value\": 68.25\n  }, {\n    \"year\": 2000,\n    \"value\": 81.88\n  }, {\n    \"year\": 2010,\n    \"value\": 85.3\n  }],\n  chudoba: [{\n    \"year\": 1820,\n    \"value\": 94.4\n  }, {\n    \"year\": 1850,\n    \"value\": 92.5\n  }, {\n    \"year\": 1870,\n    \"value\": 89.6\n  }, {\n    \"year\": 1890,\n    \"value\": 85.7\n  }, {\n    \"year\": 1910,\n    \"value\": 82.4\n  }, {\n    \"year\": 1929,\n    \"value\": 75.9\n  }, {\n    \"year\": 1950,\n    \"value\": 71.9\n  }, {\n    \"year\": 1960,\n    \"value\": 64.3\n  }, {\n    \"year\": 1970,\n    \"value\": 60.1\n  }, {\n    \"year\": 1981,\n    \"value\": 43.96\n  }, {\n    \"year\": 1984,\n    \"value\": 40.84\n  }, {\n    \"year\": 1987,\n    \"value\": 36.96\n  }, {\n    \"year\": 1990,\n    \"value\": 36.91\n  }, {\n    \"year\": 1993,\n    \"value\": 34.79\n  }, {\n    \"year\": 2000,\n    \"value\": 29.08\n  }, {\n    \"year\": 2002,\n    \"value\": 26.29\n  }, {\n    \"year\": 2005,\n    \"value\": 20.92\n  }, {\n    \"year\": 2008,\n    \"value\": 18.65\n  }, {\n    \"year\": 2010,\n    \"value\": 16.27\n  }]\n};\ngrafokres.grafokres({\n  elem: \"#graf1\",\n  data: data.doziti,\n  cutoff: 1940,\n  interval: \"decade\",\n  btnFn: postResult\n});\ngrafokres.grafokres({\n  elem: \"#graf2\",\n  data: data.demokracie,\n  cutoff: 1940,\n  interval: \"decade\",\n  btnFn: postResult,\n  yFormat: \"x + ' %'\"\n});\ngrafokres.grafokres({\n  elem: \"#graf3\",\n  data: data.katastrofy,\n  cutoff: 1950,\n  interval: \"decade\",\n  btnFn: postResult,\n  yMin: 0,\n  yMax: 1000\n});\ngrafokres.grafokres({\n  elem: \"#graf4\",\n  data: data.deti,\n  cutoff: 1940,\n  interval: \"decade\",\n  btnFn: postResult,\n  yFormat: \"x + ' %'\"\n});\ngrafokres.grafokres({\n  elem: \"#graf5\",\n  data: data.gramotnost,\n  cutoff: 1960,\n  interval: \"decade\",\n  btnFn: postResult,\n  yFormat: \"x + ' %'\"\n});\ngrafokres.grafokres({\n  elem: \"#graf6\",\n  data: data.chudoba,\n  cutoff: 1950,\n  interval: \"decade\",\n  btnFn: postResult,\n  yFormat: \"x + ' %'\"\n});\n\nvar postResult = function postResult() {\n  return console.log(\"ok!\");\n};\n\n//# sourceURL=webpack:///./js/script.js?");
+
+/***/ })
+
+/******/ });
